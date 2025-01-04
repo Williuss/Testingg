3e1718200fe0cb69773be299034ab5dec0102399
Williuss <wilsen.wj@gmail.com>
2025-01-04 22:50:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,6 @@ toll_fee = 16000
 
 st.title("Automatic Toll Payment System")
 
-# Hide disclaimer when stream is active
 if not st.session_state.get("stream_active", False):
     st.markdown(""" 
         <style>
@@ -89,16 +88,21 @@ if not st.session_state.get("stream_active", False):
             <span class="disclaimer-title">Additional Note:</span> 
             <ul>
                 <li>After conducting extensive tests, we realized that the webcam quality has a significant impact on the OCR process for license plates. A better-quality webcam will result in more accurate OCR results.</li>
+                <li>Our model was originally designed to perform plate detection and recognition in real-time using a webcam input. 
+            However, due to the limitations of Streamlit, which currently does not support real-time webcam input for processing, 
+            we have opted to use video uploads from the user instead. We apologize for any inconvenience this may cause and 
+            appreciate your understanding.</li>
             </ul>
         </div>
     """, unsafe_allow_html=True)
 
 st.sidebar.title("Control Panel")
 
-# Added guidance for user on stream process
+video_file = st.sidebar.file_uploader("Upload Video", type=["mp4", "avi", "mov", "mkv"])
+
 st.sidebar.markdown(""" 
     **To start plate detection and recognition:**
-    - Upload a video file below and click **Start Stream** to begin processing.
+    - Upload a video file above and click **Start Stream** to begin processing.
     - Once a license plate that meets the conditions is detected, click **Stop Stream** to view the results.
     - If the detected plate matches one in the database, the toll fee will be deducted from the associated balance.
 """)
@@ -107,8 +111,6 @@ start_stream = st.sidebar.button("Start Stream", key="start_stream")
 stop_stream = st.sidebar.button("Stop Stream", key="stop_stream")
 
 plate_placeholder = st.sidebar.empty()
-
-video_file = st.sidebar.file_uploader("Upload Video", type=["mp4", "avi", "mov", "mkv"])
 
 if "stream_active" not in st.session_state:
     st.session_state.stream_active = False
@@ -123,12 +125,10 @@ if stop_stream:
     st.session_state.stream_active = False
 
 if st.session_state.stream_active and video_file:
-    # Menggunakan file sementara untuk video
     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmp_video_file:
-        tmp_video_file.write(video_file.getbuffer())  # Menyimpan file video ke temporary file
-        tmp_video_file_path = tmp_video_file.name  # Mendapatkan path file sementara
+        tmp_video_file.write(video_file.getbuffer())  
+        tmp_video_file_path = tmp_video_file.name  
     
-    # Membuka file video dengan OpenCV
     cap = cv2.VideoCapture(tmp_video_file_path)
     stframe = st.empty()
 
@@ -162,8 +162,7 @@ if st.session_state.stream_active and video_file:
         stframe.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), channels="RGB")
 
     cap.release()
-    
-    
+
 if not st.session_state.stream_active and stop_stream:
     detected_texts = st.session_state.detected_texts
     if detected_texts:
@@ -186,5 +185,3 @@ if not st.session_state.stream_active and stop_stream:
     else:
         st.sidebar.write("Result : No plates detected.")
         st.subheader("Result : No plates detected.")
-    
-    
